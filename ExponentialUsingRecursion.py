0c8b51097d9b115677eb391be4dff1f50767854d
Hiranya <hiranyakudva@gmail.com>
2017-02-08 17:47:36 +0530

    Create ExponentialUsingRecursion.py

---
+++ ExponentialUsingRecursion.py
@@ -0,0 +1,11 @@
+def Power(base, exp):
+    '''
+    base: int or float.
+    exp: int >= 0
+ 
+    returns: int or float, base^exp
+    '''
+    if exp==0:
+        return 1
+    else:
+        return base*Power(base,exp-1)
